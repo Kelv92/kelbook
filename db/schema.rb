acf5cb70161b6fb75afe94d3d23163e669c46953
Yih Jiun <yjtham@hotmail.com>
2014-07-28 14:12:17 +0800

    Removed the last line of codes for roles under users table in schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20140727123813) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "role"
+    
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
